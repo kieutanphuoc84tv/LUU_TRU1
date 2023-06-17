0bc89aec5803a679d0d4b502deaa3bb14ba97205
ASUKA_RISFIN <116791022+kieutanphuoc84tv@users.noreply.github.com>
2023-06-17 09:59:22 +0700

    Update So_Phuc.cpp

--- So_Phuc.cpp
+++ So_Phuc.cpp
@@ -1,143 +1,136 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct sophuc
-{
-	float phanthuc;
-	float phanao;
-	sophuc *pnext;
-}sophuc;
-typedef struct node
-{
-	sophuc data;
-	node *pnext;
-	node *a;
-	node *n;
-	node *phanthuc;
-	node *phanao;
+struct sophuc {
+   float thuc;
+   float ao;
+   struct sophuc *next;
+};
+int main() {
+   struct sophuc *head1, *tail1, *node1;
+   struct sophuc *head2, *tail2, *node2;
+   struct sophuc *head_ketqua, *tail_ketqua, *node_ketqua;
+   int n;
+   printf("Nhap So Luong So Phuc Thu Nhat:");
+   scanf("%d", &n);
+   head1 = (struct sophuc*)malloc(sizeof(struct sophuc));
+   printf("\nNhap phan thuc 1: ");
+   scanf("%f", &(head1->thuc));
+   printf("Nhap phan ao 1: ");
+   scanf("%f", &(head1->ao));
+   tail1 = head1;
 
-}node;
-typedef struct list
-{
-	node *phead;
-	node *ptail;
-}list;
-void khoitao(list &l)
-{
-	l.phead=l.ptail=NULL;
-}
-node *taonut()
-{
-	node *p;sophuc sp;
-	p=new(node);
-	if(p!=NULL)
-	{
-		printf("\nNhap phan thuc: ");
-		scanf("%d",&sp.phanthuc);
-		printf("\nNhap phan ao: ");
-		scanf("%d",&sp.phanao);
-		p->data=sp;
-		p->pnext=NULL;
-		return p;
-	}
-	return NULL;
-}
-sophuc* createNode(int phanthuc, int phanao)
-{
-    sophuc* pnode = NULL;
-    if(phanthuc == 0)
-    {
-        return NULL;
-    }
-    pnode = (sophuc*)malloc(sizeof(sophuc));
-    if(!pnode)
-    {
-        return NULL;
-    }
-    else
-    {
-        pnode->phanthuc = phanthuc;
-        pnode->phanao = phanao;
-        pnode->pnext = NULL;
-        return pnode;
-    }
+   for (int i = 2; i <= n; i++) 
+   {
+      node1 = (struct sophuc*)malloc(sizeof(struct sophuc));
+      printf("Nhap phan thuc cua so %d: ", i);
+      scanf("%f", &(node1->thuc));
+      printf("Nhap phan ao cua so %d: ", i);
+      scanf("%f", &(node1->ao));
+      tail1->next = node1;
+      tail1 = node1;
+   }
+   tail1->next = NULL;
+   
+   printf("\nNhap So Luong So Phuc Thu Hai: ");
+   scanf("%d", &n);
+   head2 = (struct sophuc*)malloc(sizeof(struct sophuc));
+   printf("\nNhap phan thuc 1: ");
+   scanf("%f", &(head2->thuc));
+   printf("Nhap phan ao 1: ");
+   scanf("%f", &(head2->ao));
+   tail2 = head2;
+
+   for (int i = 2; i <= n; i++) {
+      node2 = (struct sophuc*)malloc(sizeof(struct sophuc));
+      printf("Nhap phan thuc cua so phuc %d: ", i);
+      scanf("%f", &(node2->thuc));
+      printf("Nhap phan ao cua so phuc %d: ", i);
+      scanf("%f", &(node2->ao));
+      tail2->next = node2;
+      tail2 = node2;
+   }
+   tail2->next = NULL;
+
+   head_ketqua = (struct sophuc*)malloc(sizeof(struct sophuc));
+   head_ketqua->thuc = head1->thuc + head2->thuc;
+   head_ketqua->ao = head1->ao + head2->ao;
+   tail_ketqua = head_ketqua;
+
+   node1 = head1->next;
+   node2 = head2->next;
+   while (node1 != NULL && node2 != NULL) 
+   {
+      node_ketqua = (struct sophuc*)malloc(sizeof(struct sophuc));
+      node_ketqua->thuc = node1->thuc + node2->thuc;
+      node_ketqua->ao = node1->ao + node2->ao;
+      tail_ketqua->next = node_ketqua;
+      tail_ketqua = node_ketqua;
+
+      node1 = node1->next;
+      node2 = node2->next;
+   }
+   tail_ketqua->next = NULL;
+   node_ketqua = head_ketqua;
+   printf("\nCong 2 So Phuc:\n");
+   while (node_ketqua != NULL) 
+   {
+      printf("%.2f + %.2fi\n", node_ketqua->thuc, node_ketqua->ao);
+      node_ketqua = node_ketqua->next;
+   }
+   head_ketqua = (struct sophuc*)malloc(sizeof(struct sophuc));
+   head_ketqua->thuc = head1->thuc - head2->thuc;
+   head_ketqua->ao = head1->ao - head2->ao;
+   tail_ketqua = head_ketqua;
+
+   node1 = head1->next;
+   node2 = head2->next;
+   while (node1 != NULL && node2 != NULL) 
+   {
+      node_ketqua = (struct sophuc*)malloc(sizeof(struct sophuc));
+      node_ketqua->thuc = node1->thuc - node2->thuc;
+      node_ketqua->ao = node1->ao - node2->ao;
+      tail_ketqua->next = node_ketqua;
+      tail_ketqua = node_ketqua;
+
+      node1 = node1->next;
+      node2 = node2->next;
+   }
+   tail_ketqua->next = NULL;
+
+   node_ketqua = head_ketqua;
+   printf("\nHieu cua 2 so phuc la:\n");
+   while (node_ketqua != NULL) 
+   {
+      printf("%.2f + %.2fi\n", node_ketqua->thuc, node_ketqua->ao);
+      node_ketqua = node_ketqua->next;
+   }
+   head_ketqua = (struct sophuc*)malloc(sizeof(struct sophuc));
+   head_ketqua->thuc = head1->thuc * head2->thuc - head1->ao * head2->ao;
+   head_ketqua->ao = head1->thuc * head2->ao + head1->ao * head2->thuc;
+   tail_ketqua = head_ketqua;
+
+   node1 = head1->next;
+   node2 = head2->next;
+   while (node1 != NULL && node2 != NULL) {
+      node_ketqua = (struct sophuc*)malloc(sizeof(struct sophuc));
+      node_ketqua->thuc = node1->thuc * node2->thuc - node1->ao * node2->ao;
+      node_ketqua->ao = node1->thuc * node2->ao + node1->ao * node2->thuc;
+      tail_ketqua->next = node_ketqua;
+      tail_ketqua = node_ketqua;
+
+      node1 = node1->next;
+      node2 = node2->next;
+   }
+   tail_ketqua->next = NULL;
+   node_ketqua = head_ketqua;
+   printf("\nTich cua 2 so phuc:\n");
+   while (node_ketqua != NULL) 
+   {
+      printf("%.2f + %.2fi\n", node_ketqua->thuc, node_ketqua->ao);
+      node_ketqua = node_ketqua->next;
+   }
+
+   return 0;
 }
 
-	void addtail(list &l,node *new_ele)
-{
-	if(l.phead==NULL)
-	{
-		l.phead=new_ele;
-		l.ptail=l.phead ;
-	}
-	else
-	{
-		l.ptail->pnext=new_ele;
-		l.ptail=new_ele;
-	}
-}
-void addfirst(list &l,node *new_ele)
-{
-	if(l.phead==NULL)
-	{
-		l.phead=new_ele;
-		l.ptail=l.phead ;
-	}
-	else
-	{
-		new_ele->pnext=l.phead;
-		l.phead=new_ele;
-	}
-}
-void addafter(list &l,node *q,node *new_ele)
-{
-	if(q!=NULL)
-	{
-		new_ele->pnext=q->pnext;
-		q->pnext=new_ele;
-		if(q==l.ptail)
-			l.ptail=new_ele;
-
-	}
-	else
-		addfirst(l,new_ele);
-}
-void taods(list &l)
-{
-	int n;node *p;
-	printf("\nNhap so luong so phuc:");
-	scanf("%d",&n);
-	for(int i=1;i<=n;i++)
-	{
-		p=taonut();
-		addfirst(l,p);
-
-	}
-}
-void duyetds(list plist)
-{
-	node* p;
-	printf("\nX= ");
-	for(p = plist.phead; p != NULL; p = p->pnext)
-    {
-        if(p->phanthuc == 0)
-            continue;
-        else if(p->phanthuc < 0)
-            printf(" %d-%di", (p->phanthuc , p->phanao));
-        else
-            printf(" %d+%di", p->phanthuc, p->phanao);
-    }
-}
-
-
-
-int main()
-{
-	list l;node* q,*new_ele;
-	khoitao(l);
-	taods(l);
-	printf("\nSo phuc da nhap la:");
-	duyetds(l);
-
-}
